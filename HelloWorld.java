d870639ed840c05c8016f9f3c4df52ce3f161672
EC2 Default User <ec2-user@ip-172-31-37-131.us-east-2.compute.internal>
2021-01-19 05:59:58 +0000

    added more lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,21 @@ public class HelloWorld  {
 Public static void main(String[] args) {
 	system.out.println("Happy sankranthi");
         system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");
+	system.out.println("Happy sankaranthi");y
    } 
 }
---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Happy Sankranti");
+    System.out.println("Happy Sankranti");
+  }
+}
