a85a7297fa2f0531bfc8722d28a5321ccec60f24
udayreddy217 <77099460+udayreddy217@users.noreply.github.com>
2021-01-26 19:09:16 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Happy Sankranti");
     System.out.println("Happy Sankranti");
+        System.out.println("hello guys enter into devops real world");
   }
 }
